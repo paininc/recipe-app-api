4ad5542fd5c588f1c31c2f7d79775a85f97b5709
Joren Janssens <jorenjanssens@me.com>
2019-12-30 18:57:13 +0100

    Bug fixes related to newer versions of Django and Django REST Framework

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+
 from rest_framework.test import APIClient
 from rest_framework import status
 
@@ -9,47 +10,47 @@ CREATE_USER_URL = reverse('user:create')
 
 
 def create_user(**params):
+    """Helper function to create new user"""
     return get_user_model().objects.create_user(**params)
 
 
-class PublicUserAPITests(TestCase):
-    """Test the user API (public)"""
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
 
     def setUp(self):
         self.client = APIClient()
 
     def test_create_valid_user_success(self):
-        """Test creating user with valid payload"""
+        """Test creating using with a valid payload is successful"""
         payload = {
-            'email': 'test@test.com',
-            'password': 'password1234',
-            'name': 'John Smith'
+            'email': 'test@londonappdev.com',
+            'password': 'testpass',
+            'name': 'name',
         }
-        resp = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(**resp.data)
-        self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn('password', resp.data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(
+            user.check_password(payload['password'])
+        )
+        self.assertNotIn('password', res.data)
 
-    def test_user_exists_already(self):
-        """Test creating a duplicate user"""
-        payload = {
-            'email': 'test@test.com',
-            'password': 'test1234567'
-        }
+    def test_user_exists(self):
+        """Test creating a user that already exists fails"""
+        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
         create_user(**payload)
-        resp = self.cient.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
-        """Test that the password is at least 5 characters"""
-        payload = {'email': 'test@test.com', 'password': 'pypy'}
-        resp = self.client.post(CREATE_USER_URL, payload)
+        """Test that password must be more than 5 characters"""
+        payload = {'email': 'test@londonappdev.com', 'password': 'pw'}
+        res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-        self.assertEqual(user_exists, False)
+        self.assertFalse(user_exists)
