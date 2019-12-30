01af1dff52c117fec29b28746c634929ca7be2aa
Joren Janssens <jorenjanssens@me.com>
2019-12-30 18:14:17 +0100

    Adding the first sections to create user API (44)

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserAPITests(TestCase):
+    """Test the user API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'password1234',
+            'name': 'John Smith'
+        }
+        resp = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**resp.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', resp.data)
+
+    def test_user_exists_already(self):
+        """Test creating a duplicate user"""
+        payload = {
+            'email': 'test@test.com',
+            'password': 'test1234567'
+        }
+        create_user(**payload)
+        resp = self.cient.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that the password is at least 5 characters"""
+        payload = {'email': 'test@test.com', 'password': 'pypy'}
+        resp = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertEqual(user_exists, False)
